423208c9613bda815daeb984b351c19b4d4f2d8a
Arghadeep <arghadeepbiswas2003@gmail.com>
2025-08-19 01:49:13 +0530

    More data added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,10 +80,10 @@ const Hero = () => {
             </motion.span>
           </motion.div>
           <h1 className="text-center text-3xl font-bold tracking-wider text-gray-500 sm:text-2xl dark:text-white transition-colors">
-            My Name is Arghadeep Biswas &
+            My Name is Arghadeep Biswas & I am an
           </h1>
           <p className="text-lg tracking-wider text-gray-700 dark:text-gray-200 transition-colors">
-            I am a full stack developer and also a chemical engineer 🤗
+            Engineer by degree, developer by curiosity, innovator by obsession.
           </p>
         </motion.div>
         <motion.div
